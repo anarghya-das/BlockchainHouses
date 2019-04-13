59c82487f401be23230ae57d352833ae76262125
anarghya-das <anarghya@buffalo.edu>
2019-04-13 10:15:28 -0400

    :art: template for the houses generated

--- src/js/app.js
+++ src/js/app.js
@@ -4,19 +4,14 @@ App = {
 
 	init: async function() {
 		// Load pets.
-		$.getJSON('../pets.json', function(data) {
-			var petsRow = $('#petsRow');
-			var petTemplate = $('#petTemplate');
+		$.getJSON('../data.json', function(data) {
+			var housesRow = $('#housesRow');
+			var houseTemplate = $('#houseTemplate');
 
 			for (i = 0; i < data.length; i++) {
-				petTemplate.find('.panel-title').text(data[i].name);
-				petTemplate.find('img').attr('src', data[i].picture);
-				petTemplate.find('.pet-breed').text(data[i].breed);
-				petTemplate.find('.pet-age').text(data[i].age);
-				petTemplate.find('.pet-location').text(data[i].location);
-				petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-				petsRow.append(petTemplate.html());
+				houseTemplate.find('.panel-title').text(data[i].streetname);
+				houseTemplate.find('img').attr('src', data[i].image);
+				housesRow.append(houseTemplate.html());
 			}
 		});
 
