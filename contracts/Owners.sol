f484dc12b2b3e582ffe86ded2b9818b3092b0c91
anarghya-das <anarghya@buffalo.edu>
2019-04-13 15:52:51 -0400

    :sparkles: added more houses

--- contracts/Owners.sol
+++ contracts/Owners.sol
@@ -1,16 +1,16 @@
 pragma solidity ^0.5.0;
 
 contract Owners {
-    address[2] public buyers;
+    address[5] public buyers;
     // adding house to the bought blockhain
 function addHouse(uint houseId) public returns (uint) {
-  require(houseId >= 0 && houseId <= 2);
+  require(houseId >= 0 && houseId <= 5);
   buyers[houseId] = msg.sender;
   return houseId;
 }
 
 // Retrieving the buyers
-function getBuyers() public view returns (address[2] memory) {
+function getBuyers() public view returns (address[5] memory) {
   return buyers;
 }
 
